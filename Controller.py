e05149455e686b38891b06800d87aaf9eafebc0f
Adam <adamrosas1972@gmail.com>
2017-11-13 18:33:34 -0800

    no sense using threads is we cannot talk to I2C devices asynchronously

--- Controller.py
+++ Controller.py
@@ -4,7 +4,7 @@ from BME280 import *
 from TSL2591 import *
 from MCP230xx import *
 from MHZ16 import *
-from time import localtime, strftime
+from time import localtime
 import curses
 import threading
 
@@ -51,6 +51,9 @@ pascals = 0
 humidity = 0
 ppm = 0
 lux = 0
+hours = 0
+minutes = 0
+seconds = 0
 
 def cooling_lockout():
   global cooling_enable
@@ -75,6 +78,10 @@ def safety_lockout():
   global run
   global ppm
   global bus_lock
+  global OFF
+  global ON
+  global EXHAUST
+  global CO2_SAFETY
   i = 0
   # wait for co2 to be over the safty limit for 5 min
   while run == 1 and i < 600:
@@ -122,21 +129,152 @@ def read_sensors():
     global ppm
     global lux
     global bus_lock
-    while run == 1:
-      while bus_lock == 1:
-        time.sleep(.01)
-      bus_lock = 1
-      degrees = air_sensor.read_temperature()
-      pascals = air_sensor.read_pressure()
-      humidity = air_sensor.read_humidity()
-      if co2_sensor.measure():
-        ppm = co2_sensor.ppm
-      full, ir = light_sensor.get_full_luminosity()
-      lux = light_sensor.calculate_lux(full, ir)
-      bus_lock = 0
-      time.sleep(.1)
+    degrees = air_sensor.read_temperature()
+    pascals = air_sensor.read_pressure()
+    humidity = air_sensor.read_humidity()
+    if co2_sensor.measure():
+      ppm = co2_sensor.ppm
+    full, ir = light_sensor.get_full_luminosity()
+    lux = light_sensor.calculate_lux(full, ir)
   except:
     pass
+
+def control_relays():
+  global degrees
+  global humidity
+  global ppm
+  global hours
+  global minutes
+  global ON
+  global OFF
+
+  global LIGHT_1
+  global LIGHT_2
+  global EXHAUST
+  global HEAT
+  global COOL
+  global CIRCULATION
+  global CO2
+  global DEHUMIDIFIER
+
+  global LIGHT_1_FLOWER
+  global LIGHT_2_FLOWER
+
+  global HEAT_HI
+  global HEAT_LOW
+  global COOL_HI
+  global COOL_LOW
+  global HUMIDITY_HI
+  global HUMIDITY_LOW
+  global CO2_LOW
+  global CO2_HIGH
+  global CO2_SAFETY
+  global cooling_enable
+  global dehumidifier_enable
+  global safety_enable
+
+# circulation --------------------------------------
+
+  relays.output(CIRCULATION, ON)
+
+# light 1 timer ------------------------------------
+
+  if LIGHT_1_FLOWER == 1:
+    if hours >= 6 and minutes >= 00:
+      if hours <=  17 and minutes <= 59:
+        relays.output(LIGHT_1, ON)
+      else:
+        relays.output(LIGHT_1, OFF)
+    else:
+      relays.output(LIGHT_1, OFF)
+  else:
+    if hours >= 6 and minutes >= 00:
+      if hours <= 23 and minutes <= 59:
+        relays.output(LIGHT_1, ON)
+      else:
+        relays.output(LIGHT_1, OFF)
+    else:
+      relays.output(LIGHT_1, OFF)
+
+# light 2 timer ------------------------------------
+
+  if LIGHT_2_FLOWER == 1:
+    if int(hours) >= 6 and int(minutes) >= 00:
+      if int(hours) <=  17 and int(minutes) <= 59:
+        relays.output(LIGHT_2, ON)
+      else:
+        relays.output(LIGHT_2, OFF)
+    else:
+	relays.output(LIGHT_2, OFF)
+  else:
+    if int(hours) >= 6 and int(minutes) >= 00:
+      if int(hours) <= 23 and int(minutes) <= 59:
+        relays.output(LIGHT_2, ON)
+      else:
+        relays.output(LIGHT_2, OFF)
+    else:
+      relays.output(LIGHT_2, OFF)
+
+# cooling -----------------------------------------
+
+  if degrees > COOL_HI:
+    if cooling_enable == 1:
+      if saftey_enable == 0:
+        relays.output(COOL, ON)
+	cooling_enable = 0
+	c = threading.Thread(target=cooling_lockout)
+	c.start()
+	relays.output(EXHAUST, ON)
+  elif degrees <= COOL_LOW:
+    relays.output(COOL, OFF)
+    if safety_enable == 0:
+      relays.output(EXHAUST, OFF)
+
+# heating ------------------------------------------
+
+  if degrees < HEAT_LOW:
+    if safety_enable == 0:
+      relays.output(HEAT, ON)
+      relays.output(EXHAUST, OFF)
+  elif degrees >= HEAT_HI:
+    relays.output(HEAT, OFF)
+
+# humidity ------------------------------------------
+
+  if humidity > HUMIDITY_HI:
+    if dehumidifier_enable == 1:
+      if safety_enable == 0:
+        relays.output(DEHUMIDIFIER, ON)
+        dehumidifier_enable = 0
+        h = threading.Thread(target=dehumidifier_lockout)
+        h.start()
+  elif  humidity <= HUMIDITY_LOW:
+    relays.output(DEHUMIDIFIER, OFF)
+
+# CO2 ------------------------------------------------
+
+  if ppm < CO2_LOW:
+    if safety_enable == 0 and co2_sensor_starting == 0:
+      relays.output(CO2, ON)
+  elif ppm >= CO2_HIGH:
+    relays.output(CO2, OFF)
+  if ppm > CO2_SAFETY:
+    s = threading.Thread(target=safety_lockout)
+    s.start()
+
+def co2_sensor_restart():
+# restart co2 sensor every 12 hours to avoid auto calabration
+  global hours
+  global co2_restart_hour
+  if hours == co2_restart_hour:
+    d = threading.Thread(target=co2_sensor_delay)
+    d.start()
+    co2_sensor.power_off()
+    co2_sensor.power_on()
+    co2_restart_hour = localtime().tm_hour + 12
+    if co2_restart_hour >= 24:
+      co2_restart_hour = co2_restart_hour - 24
+
 
 def main(stdscr):
   global run
@@ -151,18 +289,17 @@ def main(stdscr):
   global bus_lock
   global co2_sensor_starting
   global co2_restart_hour
+  global hours
+  global minutes
   try:
     relays.write_gpio([0xFF])
     relays.write_iodir([0x00])
-    r = threading.Thread(target=read_sensors)
-    d1 = threading.Thread(target=co2_sensor_delay)
-    r.start()
-    d1.start()
-    # restart co2 sensor every 12 hours to avoid auto calabration
+    d = threading.Thread(target=co2_sensor_delay)
+    d.start()
+# restart co2 sensor every 12 hours to avoid auto calabration
     co2_restart_hour = localtime().tm_hour + 12
     if co2_restart_hour >= 24:
       co2_restart_hour = co2_restart_hour - 24
-    time.sleep(1) # give sensors a sec to start outputing data
     stdscr.nodelay(1)
     while 1:
       k = stdscr.getch()
@@ -180,115 +317,17 @@ def main(stdscr):
       minutes = localtime().tm_min
       seconds = localtime().tm_sec
 
-# restart co2 sensor to avoid auto calibration -----
-
-      if hours == co2_restart_hour:
-        d2 = threading.Thread(target=co2_sensor_delay)
-        d2.start()
-        while bus_lock == 1:
-          time.sleep(.01)
-        bus_lock = 1
-        co2_sensor.power_off()
-        co2_sensor.power_on()
-        bus_lock = 0
-        co2_restart_hour = localtime().tm_hour + 12
-        if co2_restart_hour >= 24:
-          co2_restart_hour = co2_restart_hour - 24
-
 # lock the I2C bus ---------------------------------
 
       while bus_lock == 1:
         time.sleep(.01)
       bus_lock = 1
 
-# circulation --------------------------------------
+# do stuff -------------------------------------------
 
-      relays.output(CIRCULATION, ON)
-
-# light 1 timer ------------------------------------
-
-      if LIGHT_1_FLOWER == 1:
-        if int(hours) >= 6 and int(minutes) >= 00:
-          if int(hours) <=  17 and int(minutes) <= 59:
-            relays.output(LIGHT_1, ON)
-          else:
-            relays.output(LIGHT_1, OFF)
-        else:
-           relays.output(LIGHT_1, OFF)
-      else:
-        if int(hours) >= 6 and int(minutes) >= 00:
-          if int(hours) <= 23 and int(minutes) <= 59:
-            relays.output(LIGHT_1, ON)
-          else:
-            relays.output(LIGHT_1, OFF)
-        else:
-            relays.output(LIGHT_1, OFF)
-
-# light 2 timer ------------------------------------
-
-      if LIGHT_2_FLOWER == 1:
-        if int(hours) >= 6 and int(minutes) >= 00:
-          if int(hours) <=  17 and int(minutes) <= 59:
-            relays.output(LIGHT_2, ON)
-          else:
-            relays.output(LIGHT_2, OFF)
-        else:
-          relays.output(LIGHT_2, OFF)
-      else:
-        if int(hours) >= 6 and int(minutes) >= 00:
-          if int(hours) <= 23 and int(minutes) <= 59:
-            relays.output(LIGHT_2, ON)
-          else:
-            relays.output(LIGHT_2, OFF)
-        else:
-          relays.output(LIGHT_2, OFF)
-
-# cooling -----------------------------------------
-
-      if degrees > COOL_HI:
-        if cooling_enable == 1:
-          if saftey_enable == 0:
-            relays.output(COOL, ON)
-            cooling_enable = 0
-            c = threading.Thread(target=cooling_lockout)
-            c.start()
-            relays.output(EXHAUST, ON)
-      elif degrees <= COOL_LOW:
-          relays.output(COOL, OFF)
-          if safety_enable == 0:
-            relays.output(EXHAUST, OFF)
-
-# heating ------------------------------------------
-
-      if degrees < HEAT_LOW:
-        if safety_enable == 0:
-          relays.output(HEAT, ON)
-          relays.output(EXHAUST, OFF)
-      elif degrees >= HEAT_HI:
-        relays.output(HEAT, OFF)
-
-# humidity ------------------------------------------
-
-      if humidity > HUMIDITY_HI:
-        if dehumidifier_enable == 1:
-          if safety_enable == 0:
-            relays.output(DEHUMIDIFIER, ON)
-            dehumidifier_enable = 0
-            h = threading.Thread(target=dehumidifier_lockout)
-            h.start()
-      elif  humidity <= HUMIDITY_LOW:
-          relays.output(DEHUMIDIFIER, OFF)
-
-# CO2 ------------------------------------------------
-
-      if ppm < CO2_LOW:
-        if safety_enable == 0 and co2_sensor_starting == 0:
-          relays.output(CO2, ON)
-      elif ppm >= CO2_HIGH:
-        relays.output(CO2, OFF)
-      if ppm > CO2_SAFETY:
-        s = threading.Thread(target=safety_lockout)
-        s.start()
+      read_sensors()
+      control_relays()
+      co2_sensor_restart()
 
 # unlock the I2C bus ---------------------------------
 
