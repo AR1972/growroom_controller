f13a58273f422bc45296701f79c04fc859a53ecd
Adam <adamrosas1972@gmail.com>
2017-11-15 17:05:05 -0800

    allow a 30 sec delay for sensors to settle before turning on equipment track the state ON/OFF of equipment

--- Controller.py
+++ Controller.py
@@ -20,6 +20,15 @@ CIRCULATION = 5
 CO2 = 6
 DEHUMIDIFIER = 7
 
+light1_on = 0
+light2_on = 0
+exhaust_on = 0
+heat_on = 0
+cool_on = 0
+circulation_on = 0
+co2_on = 0
+dehumidifier_on = 0
+
 LIGHT_1_FLOWER = 0 # 1 = 12 hours, 0 = 18 hours
 LIGHT_2_FLOWER = 0 # 1 = 12 hours, 0 = 18 hours
 LIGHT_1_ON = 7
@@ -69,6 +78,8 @@ lux = 0
 hours = 0
 minutes = 0
 seconds = 0
+delay = 1
+co2_sensor_restart_count = 0
 
 def cooling_lockout():
     global cooling_enable
@@ -97,6 +108,7 @@ def safety_lockout():
     global ON
     global EXHAUST
     global CO2_SAFETY
+    global exhaust_on
     try:
         i = 0
         # wait for co2 to be over the safty limit for 5 min
@@ -111,6 +123,7 @@ def safety_lockout():
                 time.sleep(.01)
             bus_lock = 1
             relays.output(EXHAUST, ON)
+            exhaust_on = 1
             bus_lock = 0
             safety_enable = 1
         i = 0
@@ -124,6 +137,7 @@ def safety_lockout():
                 time.sleep(.01)
             bus_lock = 1
             relays.output(EXHAUST, OFF)
+            exhaust_on = 0
             bus_lock = 0
             safety_enable = 0
     except:
@@ -140,6 +154,16 @@ def co2_sensor_delay():
         time.sleep(.5)
         i += 1
     co2_sensor_starting = 0
+
+def startup_delay():
+    global run
+    global delay
+    i = 0
+    while run == 1 and i <= 60:
+        delay = 1
+        time.sleep(.5)
+        i += 1
+    delay = 0
 
 def read_sensors():
     global degrees
@@ -194,10 +218,20 @@ def control_relays():
     global dehumidifier_enable
     global safety_enable
     global co2_sensor_starting
+    global delay
+    global light1_on
+    global light2_on
+    global exhaust_on
+    global heat_on
+    global cool_on
+    global circulation_on
+    global co2_on
+    global dehumidifier_on
     try:
 # circulation --------------------------------------
 
         relays.output(CIRCULATION, ON)
+        circulation_on = 1
 
 # light 1 timer ------------------------------------
 
@@ -205,18 +239,24 @@ def control_relays():
             if hours >= LIGHT_1_ON and minutes >= 00:
                 if hours <= LIGHT_1_OFF-1 and minutes <= 59:
                     relays.output(LIGHT_1, ON)
+                    light1_on = 1
                 else:
                     relays.output(LIGHT_1, OFF)
+                    light1_on = 0
             else:
                 relays.output(LIGHT_1, OFF)
+                light1_on = 0
         else:
             if hours >= LIGHT_1_OFF and minutes >= 00:
                 if hours <= LIGHT_1_ON-1 and minutes <= 59:
                     relays.output(LIGHT_1, OFF)
+                    light1_on = 0
                 else:
                     relays.output(LIGHT_1, ON)
+                    light1_on = 1
             else:
                 relays.output(LIGHT_1, ON)
+                light1_on = 1
 
 # light 2 timer ------------------------------------
 
@@ -224,62 +264,79 @@ def control_relays():
             if hours >= LIGHT_2_ON and minutes >= 00:
                 if hours <= LIGHT_2_OFF-1 and minutes <= 59:
                     relays.output(LIGHT_2, ON)
+                    light2_on = 1
                 else:
                     relays.output(LIGHT_2, OFF)
+                    light2_on = 0
             else:
                 relays.output(LIGHT_2, OFF)
+                light2_on = 0
         else:
             if hours >= LIGHT_2_OFF and minutes >= 00:
                 if hours <= LIGHT_2_ON-1 and minutes <= 59:
                     relays.output(LIGHT_2, OFF)
+                    light2_on = 0
                 else:
                     relays.output(LIGHT_2, ON)
+                    light2_on = 1
             else:
                 relays.output(LIGHT_2, ON)
+                light2_on = 1
 
 # cooling -----------------------------------------
 
         if degrees > COOL_HI:
             if cooling_enable == 1:
-                if saftey_enable == 0:
+                if saftey_enable == 0 and delay == 0:
                     relays.output(COOL, ON)
+                    cool_on = 1
                     cooling_enable = 0
                     c = threading.Thread(target=cooling_lockout)
                     c.start()
                     relays.output(EXHAUST, ON)
+                    exhaust_on = 1
         elif degrees <= COOL_LOW:
             relays.output(COOL, OFF)
+            cool_on = 0
             if safety_enable == 0:
                 relays.output(EXHAUST, OFF)
+                exhaust_on = 0
 
 # heating ------------------------------------------
 
         if degrees < HEAT_LOW:
-            if safety_enable == 0:
+            if safety_enable == 0 and delay == 0:
                 relays.output(HEAT, ON)
+                heat_on = 1
                 relays.output(EXHAUST, OFF)
+                exhaust_on = 0
         elif degrees >= HEAT_HI:
             relays.output(HEAT, OFF)
+            heat_on = 0
 
 # humidity ------------------------------------------
 
         if humidity > HUMIDITY_HI:
             if dehumidifier_enable == 1:
-                if safety_enable == 0:
+                if safety_enable == 0 and delay == 0:
                     relays.output(DEHUMIDIFIER, ON)
+                    dehumidifier_on = 1
                     dehumidifier_enable = 0
                     h = threading.Thread(target=dehumidifier_lockout)
                     h.start()
         elif humidity <= HUMIDITY_LOW:
             relays.output(DEHUMIDIFIER, OFF)
+            dehumidifier_on = 0
 
 # CO2 ------------------------------------------------
 
         if ppm < CO2_LOW:
             if safety_enable == 0 and co2_sensor_starting == 0:
                 relays.output(CO2, ON)
+                co2_on = 1
         elif ppm >= CO2_HIGH:
             relays.output(CO2, OFF)
+            co2_on = 0
         if ppm > CO2_SAFETY:
             s = threading.Thread(target=safety_lockout)
             s.start()
@@ -292,12 +349,14 @@ def co2_sensor_restart():
 # restart co2 sensor every 12 hours to avoid auto calabration
     global hours
     global co2_restart_hour
+    global co2_sensor_restart_count
     try:
         if hours == co2_restart_hour:
             d = threading.Thread(target=co2_sensor_delay)
             d.start()
             co2_sensor.power_off()
             co2_sensor.power_on()
+            co2_sensor_restart_count = co2_sensor_restart_count + 1
             co2_restart_hour = localtime().tm_hour + 12
             if co2_restart_hour >= 24:
                 co2_restart_hour = co2_restart_hour - 24
@@ -321,11 +380,21 @@ def main(stdscr):
     global co2_restart_hour
     global hours
     global minutes
+    global light1_on
+    global light2_on
+    global exhaust_on
+    global heat_on
+    global cool_on
+    global circulation_on
+    global co2_on
+    global dehumidifier_on
     try:
         relays.write_gpio([0xFF])
         relays.write_iodir([0x00])
         d = threading.Thread(target=co2_sensor_delay)
+        q = threading.Thread(target=startup_delay)
         d.start()
+        q.start()
 # restart co2 sensor every 12 hours to avoid auto calabration
         co2_restart_hour = localtime().tm_hour + 12
         if co2_restart_hour >= 24:
@@ -372,7 +441,8 @@ def main(stdscr):
             stdscr.addstr(3, 0, 'Humidity  = %0.2f %%' % humidity)
             stdscr.addstr(4, 0, 'Light     = %s lux' % lux)
             stdscr.addstr(5, 0, 'CO2       = %s ppm' % ppm)
-            stdscr.addstr(7, 0, 'Press Q key to exit...')
+            stdscr.addstr(7, 0, '[Light 1 = %s][Light 2 = %s][Exhaust = %s][Heat = %s][Cool = %s][Circulation = %s][CO2 = %s][Dehumidifier = %s]' % (light1_on,light2_on,exhaust_on,heat_on,cool_on,circulation_on,co2_on,dehumidifier_on))
+            stdscr.addstr(9, 0, 'Press Q key to exit...')
             stdscr.refresh()
 
     except Exception as e:
